1abf3648b02fdadf0ae7775ea83468b3799ce6af
Fuad <kamilovfuad@gmail.com>
2021-12-24 13:02:18 +0400

    Figure out the relationship between Airport and Flight models, g controller for flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,3 +1,4 @@
 class Airport < ApplicationRecord
-    has_many :flights
+    has_many :departuring_flights, class_name: "Flight", inverse_of: :departure_airport
+    has_many :arriving_flights, class_name: "Flight", inverse_of: :arrival_airport
 end
