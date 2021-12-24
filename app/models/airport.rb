2287d7be0bbd34558a0b89d38b284dbbb5df9319
Fuad <kamilovfuad@gmail.com>
2021-12-24 19:19:55 +0400

    Specify foreign keys for airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-    has_many :departuring_flights, class_name: "Flight", inverse_of: :departure_airport
-    has_many :arriving_flights, class_name: "Flight", inverse_of: :arrival_airport
+    has_many :departuring_flights, class_name: "Flight", inverse_of: :departure_airport, foreign_key: :departure_airport_id
+    has_many :arriving_flights, class_name: "Flight", inverse_of: :arrival_airport, foreign_key: :arrival_airport_id
 end
